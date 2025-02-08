3382aa61c83e59035732315e92ac07fc25dc1032
Arkadiusz Lis <areklis909@gmail.com>
2025-02-08 12:40:13 +0100

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GalleryModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gallery-test';
+
+  items: GalleryItem[] = [
+    new ImageItem({
+      src: '/assets/gallery.jpg'
+    }),
+    new ImageItem({
+      src: '/assets/contact.jpg'
+    }),
+  ];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { GalleryModule, GalleryConfig, GALLERY_CONFIG } from 'ng-gallery';
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(),
+  importProvidersFrom(GalleryModule), {
+
+    provide: GALLERY_CONFIG,
+    useValue: {
+      imageSize: 'cover'
+    } as GalleryConfig
+
+  }
+
+  ]
 };
